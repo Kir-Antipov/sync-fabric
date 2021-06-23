aa14263e7a1d724c9ca0a6c3845445f1e2f2b4ae
Kir_Antipov <kp.antipov@gmail.com>
2021-06-23 05:39:23 +0300

    Implemented mod initializer

---
+++ src/main/java/me/kirantipov/mods/sync/Sync.java
@@ -0,0 +1,23 @@
+package me.kirantipov.mods.sync;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+
+public class Sync implements ModInitializer, ClientModInitializer {
+    public static final String MOD_ID = "sync";
+
+    public static Identifier locate(String location) {
+        return new Identifier(MOD_ID, location);
+    }
+
+    @Override
+    public void onInitialize() {
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public void onInitializeClient() {
+    }
+}
