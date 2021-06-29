004ed01313a19571dadb9bee50fc01dafabc115d
Kir_Antipov <kp.antipov@gmail.com>
2021-06-29 14:43:01 +0300

    Collected frequently used radian values in one place

---
+++ src/main/java/me/kirantipov/mods/sync/util/math/Radians.java
@@ -0,0 +1,21 @@
+package me.kirantipov.mods.sync.util.math;
+
+import net.minecraft.util.math.MathConstants;
+
+public final class Radians {
+    public static final float R_PI = MathConstants.PI;
+    public static final float R_2_PI = 2F * R_PI;
+
+    public static final float R_PI_128 = R_PI / 128;
+    public static final float R_PI_64 = R_PI / 64;
+    public static final float R_PI_32 = R_PI / 32;
+    public static final float R_PI_16 = R_PI / 16;
+    public static final float R_PI_8 = R_PI / 8;
+    public static final float R_PI_6 = R_PI / 6;
+    public static final float R_PI_4 = R_PI / 4;
+    public static final float R_PI_3 = R_PI / 3;
+    public static final float R_PI_2 = R_PI / 2;
+
+    public static final float R_2_PI_3 = 2 * R_PI / 3;
+    public static final float R_3_PI_2 = 3 * R_PI / 2;
+}
