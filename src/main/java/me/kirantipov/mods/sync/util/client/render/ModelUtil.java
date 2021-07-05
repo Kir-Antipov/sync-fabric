602886ed8aa49c4d0a32c0a78a03cf2a95925bfa
Kir_Antipov <kp.antipov@gmail.com>
2021-07-05 13:48:43 +0300

    Implemented methods that can copy ModelPart or split it into separate voxels

---
+++ src/main/java/me/kirantipov/mods/sync/util/client/render/ModelUtil.java
@@ -0,0 +1,45 @@
+package me.kirantipov.mods.sync.util.client.render;
+
+import me.kirantipov.mods.sync.util.math.Voxel;
+import me.kirantipov.mods.sync.util.math.VoxelIterator;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.model.ModelPart;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+@Environment(EnvType.CLIENT)
+public final class ModelUtil {
+    public static ModelPart copy(ModelPart original) {
+        ModelPart copy = new ModelPart(original.cuboids, original.children);
+        copy.copyTransform(original);
+        return copy;
+    }
+
+    public static Stream<Voxel> asVoxels(ModelPart part) {
+        return asVoxels(0, 0, 0, part);
+    }
+
+    public static Stream<Voxel> asVoxels(float x, float y, float z, ModelPart part) {
+        final float pivotX = x + part.pivotX;
+        final float pivotY = y + part.pivotY;
+        final float pivotZ = z + part.pivotZ;
+        return Stream.concat(
+            part.cuboids.stream().flatMap(cuboid -> asVoxels(pivotX, pivotY, pivotZ, cuboid)),
+            part.children.values().stream().flatMap(p -> asVoxels(pivotX, pivotY, pivotZ, p))
+        );
+    }
+
+    public static Stream<Voxel> asVoxels(float x, float y, float z, ModelPart.Cuboid cuboid) {
+        int sizeX = (int)(cuboid.maxX - cuboid.minX);
+        int sizeY = (int)(cuboid.maxY - cuboid.minY);
+        int sizeZ = (int)(cuboid.maxZ - cuboid.minZ);
+        Iterator<Voxel> iterator = new VoxelIterator(x, y, z, sizeX, sizeY, sizeZ);
+        Spliterator<Voxel> spliterator = Spliterators.spliteratorUnknownSize(iterator, 0);
+        return StreamSupport.stream(spliterator, false);
+    }
+}
