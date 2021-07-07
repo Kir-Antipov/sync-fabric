54e8246a60df5b0f6be09aacdee8d57affadd088
Kir_Antipov <kp.antipov@gmail.com>
2021-07-07 14:43:46 +0300

    I need model matrix to make my shader work, so...

---
+++ src/main/java/me/kirantipov/mods/sync/mixin/MixinWorldRenderer.java
@@ -0,0 +1,30 @@
+package me.kirantipov.mods.sync.mixin;
+
+import me.kirantipov.mods.sync.client.render.MatrixStackStorage;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Matrix4f;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(value = WorldRenderer.class, priority = 1001)
+public class MixinWorldRenderer {
+    @Inject(method = "render", at = @At("HEAD"))
+    private void render(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, CallbackInfo ci) {
+        MatrixStackStorage.saveModelMatrixStack(matrices);
+    }
+}
