e44662cd1c8db7270777c3d1190098d6e595db36
Kir_Antipov <kp.antipov@gmail.com>
2021-07-07 13:08:26 +0300

    Added container for custom render layers

---
+++ src/main/java/me/kirantipov/mods/sync/client/render/CustomRenderLayer.java
@@ -0,0 +1,13 @@
+package me.kirantipov.mods.sync.client.render;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexFormat;
+
+@Environment(EnvType.CLIENT)
+public final class CustomRenderLayer extends RenderLayer {
+    private CustomRenderLayer(String name, VertexFormat vertexFormat, VertexFormat.DrawMode drawMode, int expectedBufferSize, boolean hasCrumbling, boolean translucent, Runnable startAction, Runnable endAction) {
+        super(name, vertexFormat, drawMode, expectedBufferSize, hasCrumbling, translucent, startAction, endAction);
+    }
+}
