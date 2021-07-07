ed841e1c2bada3d91dd874ce8172124a6d529ee9
Kir_Antipov <kp.antipov@gmail.com>
2021-07-07 13:14:32 +0300

    Implemented RenderLayer for voxel rendering

--- src/main/java/me/kirantipov/mods/sync/client/render/CustomRenderLayer.java
+++ src/main/java/me/kirantipov/mods/sync/client/render/CustomRenderLayer.java
@@ -7,7 +7,21 @@ import net.minecraft.client.render.VertexFormat;
 
 @Environment(EnvType.CLIENT)
 public final class CustomRenderLayer extends RenderLayer {
+    private static final Shader VOXEL_SHADER;
+
+    private static final RenderLayer VOXELS;
+
     private CustomRenderLayer(String name, VertexFormat vertexFormat, VertexFormat.DrawMode drawMode, int expectedBufferSize, boolean hasCrumbling, boolean translucent, Runnable startAction, Runnable endAction) {
         super(name, vertexFormat, drawMode, expectedBufferSize, hasCrumbling, translucent, startAction, endAction);
+    }
+
+    public static RenderLayer getVoxels() {
+        return VOXELS;
+    }
+
+    static {
+        VOXEL_SHADER = new Shader(CustomGameRenderer::getRenderTypeVoxelShader);
+
+        VOXELS = of("voxels", CustomVertexFormats.POSITION_COLOR_OVERLAY_LIGHT_NORMAL, VertexFormat.DrawMode.QUADS, 256, false, false, RenderLayer.MultiPhaseParameters.builder().shader(VOXEL_SHADER).transparency(NO_TRANSPARENCY).cull(DISABLE_CULLING).lightmap(ENABLE_LIGHTMAP).overlay(ENABLE_OVERLAY_COLOR).build(true));
     }
 }
