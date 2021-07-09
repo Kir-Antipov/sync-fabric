b27c05f96859419adf42a9f9873e0ef22ad61698
Kir_Antipov <kp.antipov@gmail.com>
2021-07-09 15:45:08 +0300

    Implemented `draw` method to... draw some basic things

---
+++ src/main/java/me/kirantipov/mods/sync/util/client/render/RenderSystemUtil.java
@@ -0,0 +1,34 @@
+package me.kirantipov.mods.sync.util.client.render;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import me.kirantipov.mods.sync.util.math.Radians;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.Vec3f;
+
+import java.util.function.Consumer;
+
+@Environment(EnvType.CLIENT)
+public final class RenderSystemUtil {
+    public static void draw(Consumer<VertexConsumer> consumer, VertexFormat.DrawMode drawMode, VertexFormat format) {
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.disableTexture();
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        bufferBuilder.begin(drawMode, format);
+        consumer.accept(bufferBuilder);
+        bufferBuilder.end();
+        BufferRenderer.draw(bufferBuilder);
+
+        RenderSystem.disableBlend();
+        RenderSystem.enableTexture();
+    }
+}
