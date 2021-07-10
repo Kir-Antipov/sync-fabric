ea4e1d2652ea4d8176575c96c15473183fd15fce
Kir_Antipov <kp.antipov@gmail.com>
2021-07-10 08:44:13 +0300

    Added methods to draw rectangles

--- src/main/java/me/kirantipov/mods/sync/util/client/render/RenderSystemUtil.java
+++ src/main/java/me/kirantipov/mods/sync/util/client/render/RenderSystemUtil.java
@@ -24,6 +24,45 @@ public final class RenderSystemUtil {
         draw(consumer, VertexFormat.DrawMode.TRIANGLE_STRIP, format);
     }
 
+    public static void drawRectangle(MatrixStack matrices, float x, float y, float width, float height, float borderRadius, float scale, float rotation, float step, float r, float g, float b, float a) {
+        drawTriangleStrip(consumer -> drawRectangle(matrices, consumer, x, y, width, height, borderRadius, scale, rotation, step, r, g, b, a));
+    }
+
+    public static void drawRectangle(MatrixStack matrices, VertexConsumer consumer, float x, float y, float width, float height, float borderRadius, float scale, float rotation, float step, float r, float g, float b, float a) {
+        matrices.push();
+        matrices.translate(x, y, 0);
+        matrices.multiply(Vec3f.POSITIVE_Z.getRadialQuaternion(rotation));
+        matrices.scale(scale, scale, 1);
+        matrices.translate(-x, -y, 0);
+        Matrix4f matrix = matrices.peek().getModel();
+
+        drawQuadrant(matrix, consumer, x + width - borderRadius, y + height - borderRadius, borderRadius, 0, step, r, g, b, a);
+        drawQuadrant(matrix, consumer, x + borderRadius, y + height - borderRadius, borderRadius, 1, step, r, g, b, a);
+        drawQuadrant(matrix, consumer, x + borderRadius, y + borderRadius, borderRadius, 2, step, r, g, b, a);
+        drawQuadrant(matrix, consumer, x + width - borderRadius, y + borderRadius, borderRadius, 3, step, r, g, b, a);
+
+        consumer.vertex(matrix, x + width, y + height - borderRadius, 0).color(r, g, b, a).next();
+        consumer.vertex(matrix, x + borderRadius, y + borderRadius, 0).color(r, g, b, a).next();
+        consumer.vertex(matrix, x + borderRadius, y + height - borderRadius, 0).color(r, g, b, a).next();
+
+        matrices.pop();
+    }
+
+    private static void drawQuadrant(Matrix4f matrix, VertexConsumer consumer, float cX, float cY, float radius, int index, float step, float r, float g, float b, float a) {
+        float start = Radians.R_PI_2 * index;
+        float end = Radians.R_PI_2 * (index + 1);
+        for (float i = start; i < end; i += step) {
+            float x = radius * (float)Math.cos(i) + cX;
+            float y = radius * (float)Math.sin(i) + cY;
+            consumer.vertex(matrix, x, y, 0).color(r, g, b, a).next();
+            consumer.vertex(matrix, cX, cY, 0).color(r, g, b, a).next();
+        }
+        float x = radius * (float)Math.cos(end) + cX;
+        float y = radius * (float)Math.sin(end) + cY;
+        consumer.vertex(matrix, x, y, 0).color(r, g, b, a).next();
+        consumer.vertex(matrix, cX, cY, 0).color(r, g, b, a).next();
+    }
+
     public static void draw(Consumer<VertexConsumer> consumer, VertexFormat.DrawMode drawMode, VertexFormat format) {
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
