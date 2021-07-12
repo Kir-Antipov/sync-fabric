e9ce35cfb51dc5463556ec2c588cc6559e48900f
Kir_Antipov <kp.antipov@gmail.com>
2021-07-12 21:42:26 +0300

    Fixed player rendering when PersistentCameraEntity is active

--- src/main/java/me/kirantipov/mods/sync/mixin/MixinWorldRenderer.java
+++ src/main/java/me/kirantipov/mods/sync/mixin/MixinWorldRenderer.java
@@ -23,6 +23,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Environment(EnvType.CLIENT)
 @Mixin(value = WorldRenderer.class, priority = 1001)
 public class MixinWorldRenderer {
+    @Final
+    @Shadow
+    private MinecraftClient client;
+
+    /**
+     * This method forces renderer to render the player when they aren't a camera entity.
+     */
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;getFocusedEntity()Lnet/minecraft/entity/Entity;", ordinal = 3), require = 1)
+    private Entity getFocusedEntity(Camera camera) {
+        ClientPlayerEntity player = this.client.player;
+        if (player != null && player != this.client.getCameraEntity() && !player.isSpectator()) {
+            return player;
+        }
+        return camera.getFocusedEntity();
+    }
+
     @Inject(method = "render", at = @At("HEAD"))
     private void render(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, CallbackInfo ci) {
         MatrixStackStorage.saveModelMatrixStack(matrices);
