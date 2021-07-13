c9e6f6df31d116fe345b51c89c95c979948021b9
Kir_Antipov <kp.antipov@gmail.com>
2021-07-13 10:29:27 +0300

    Made BooleanAnimator (it smoothly changes its progress from 0 to 1 and from 1 to 0 depeneding on provided boolean value)

---
+++ src/main/java/me/kirantipov/mods/sync/block/entity/BooleanAnimator.java
@@ -0,0 +1,36 @@
+package me.kirantipov.mods.sync.block.entity;
+
+import net.minecraft.util.math.MathHelper;
+
+public class BooleanAnimator {
+    private boolean value;
+    private float progress;
+    private float lastProgress;
+    private final float stepDelta;
+
+    public BooleanAnimator(boolean value) {
+        this(value, 0.1F);
+    }
+
+    public BooleanAnimator(boolean value, float stepDelta) {
+        this.value = value;
+        this.stepDelta = stepDelta;
+    }
+
+    public void setValue(boolean value) {
+        this.value = value;
+    }
+
+    public float getProgress(float delta) {
+        return MathHelper.lerp(delta, this.lastProgress, this.progress);
+    }
+
+    public void step() {
+        this.lastProgress = this.progress;
+        if (!this.value && this.progress > 0F) {
+            this.progress = Math.max(this.progress - stepDelta, 0F);
+        } else if (this.value && this.progress < 1F) {
+            this.progress = Math.min(this.progress + stepDelta, 1F);
+        }
+    }
+}
