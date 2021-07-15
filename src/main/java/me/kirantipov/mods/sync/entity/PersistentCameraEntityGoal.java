2f53def5d6dc08388a000c01a0234b4fcbca4f75
Kir_Antipov <kp.antipov@gmail.com>
2021-07-15 19:43:05 +0300

    Added new camera goal
    Now camera can soar into the sky

--- src/main/java/me/kirantipov/mods/sync/entity/PersistentCameraEntityGoal.java
+++ src/main/java/me/kirantipov/mods/sync/entity/PersistentCameraEntityGoal.java
@@ -9,6 +9,12 @@ import java.util.function.Consumer;
 
 @Environment(EnvType.CLIENT)
 public class PersistentCameraEntityGoal {
+    public static final double MAX_DISTANCE = 25;
+    public static final long PHASE_DELAY = 200;
+    public static final double MAX_Y = DimensionType.OVERWORLD.getHeight() * 1.01;
+    public static final long MIN_PHASE_DURATION = 400;
+    public static final long MAX_PHASE_DURATION = 2500;
+
     public final Vec3d pos;
     public final float yaw;
     public final float pitch;
@@ -56,5 +62,29 @@ public class PersistentCameraEntityGoal {
 
     public static PersistentCameraEntityGoal create(Vec3d pos, float yaw, float pitch, long delay, long duration, Consumer<PersistentCameraEntity> onTransitionFinished) {
         return new PersistentCameraEntityGoal(pos, yaw, pitch, delay, duration, onTransitionFinished);
+    }
+
+    public static PersistentCameraEntityGoal stairwayToHeaven(BlockPos start, Direction startFacing, BlockPos target, Consumer<PersistentCameraEntity> onTransitionFinished) {
+        return stairwayToHeaven(start, startFacing, MAX_Y, target, MIN_PHASE_DURATION, MAX_PHASE_DURATION, PHASE_DELAY, MAX_DISTANCE, onTransitionFinished);
+    }
+
+    public static PersistentCameraEntityGoal stairwayToHeaven(BlockPos start, Direction startFacing, double y, BlockPos target, long firstPhaseDuration, long secondPhaseDuration, long phaseDelay, double maxDistance, Consumer<PersistentCameraEntity> onTransitionFinished) {
+        double dX = target.getX() - start.getX();
+        double dZ = target.getZ() - start.getZ();
+        double horizontalDistance = Math.sqrt(dX * dX + dZ * dZ);
+        if (horizontalDistance > maxDistance) {
+            double factor = maxDistance / horizontalDistance;
+            target = new BlockPos(start.add(dX * factor, 0, dZ * factor));
+        }
+
+        float yaw = startFacing.asRotation();
+        float pitch = 90;
+        BlockPos pos0 = start.offset(startFacing.getOpposite());
+        Vec3d pos1 = new Vec3d(start.getX() + target.getX(), y * 2, start.getZ() + target.getZ()).multiply(0.5);
+
+        PersistentCameraEntityGoal goal0 = create(pos0, yaw, 0, firstPhaseDuration);
+        PersistentCameraEntityGoal goal1 = create(pos1, yaw, pitch, phaseDelay, secondPhaseDuration, onTransitionFinished);
+
+        return goal0.then(goal1);
     }
 }
